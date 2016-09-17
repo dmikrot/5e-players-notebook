82ec6b467a27fb67353bb2893d2c341356c1e6ec
Donovan Mikrot <donovan@donovan.mn>
2016-09-17 12:54:42 -0500

    Configure some basic routing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  configureRouter(config, router) {
+    config.title = 'Player\'s Notebook';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([{
+      route: '', redirect: 'characters'
+    }, {
+      route: 'characters', moduleId: 'character/list', title: 'Characters', nav: true 
+    }]);
   }
 }
---
+++ src/character/list.js
@@ -0,0 +1,13 @@
+export class CharacterList {
+  constructor() {
+    this.characters = [{
+      name: 'Droop',
+      class: 'Ranger',
+      level: 4
+    }, {
+      name: 'ali\'Emaravel Nailo',
+      class: 'Ranger',
+      level: 8
+    }];
+  }
+}
